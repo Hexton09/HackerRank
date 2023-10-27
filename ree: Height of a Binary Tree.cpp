9ea1f5f09c20c17c6fbd509554a9041b4cdcba3c
Rahul Singh Nagesh <98824774+Hexton09@users.noreply.github.com>
2023-10-27 13:24:17 +0530

    Update ree: Height of a Binary Tree.cpp

--- ree: Height of a Binary Tree.cpp
+++ ree: Height of a Binary Tree.cpp
@@ -2,12 +2,12 @@ int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-int getHeight(struct node* root) {
+int height(Node* root) {
     if (root == NULL) {
         return -1;
     } else {
-        int leftHeight = getHeight(root->left);
-        int rightHeight = getHeight(root->right);
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
         return 1 + max(leftHeight, rightHeight);
     }
 }
